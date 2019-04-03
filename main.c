27d56015062c15368cd798df97f3d54f1caea62f
LowlingLife <49157065+LowlingLife@users.noreply.github.com>
2019-04-03 14:06:43 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,16 @@
+#include <stdio.h>
+#include "function.h"
+#include "filehandler.h"
+
 int main()
 {
+	praktikum list[12][5][4];
+    	char last[12][5][4][2];
+    	list[0][0][0].matkul = 2;
+    	list[0][0][0].group = 5;
+    	last[0][0][0][0] = 'A';
+    	last[0][0][0][1] = 'B';
+    	saveAsisten(last);
+    	savePraktikum(list);
 	return 0;
 }
