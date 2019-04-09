c60f1420186b260b8ac6ac58f44c3f44f82f94f7
LowlingLife <49157065+LowlingLife@users.noreply.github.com>
2019-04-09 22:35:17 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,51 +1,108 @@
+//***********************************************************//
+//                      [ Source Code ]
+//
+// Institution       : Institut Teknologi Bandung
+// Name              : 
+// Module            : Tugas Besar
+// Problem           : Main Program
+// File Name         : main.c
+// Dependency        : stdio.h, stdlib.h, math.h, 
+//
+// Status:
+//		1.  Arief Himanto / 13217076
+//		2.  Minangkara Ciandi Rengga / 13217087
+//		3.  Kevin Sutardi / 13217088
+//		4.  Ramadhita Umitaibatin / 18317008
+//		5.  Muhammad Harun Al Rasyid / 18317009
+// 
+//***********************************************************//
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include "function.h"
 #include "filehandler.h"
+#include "function.h"
 
 int main()
 {
-    int i, j, k;
-    praktikum list[12][5][4];
-    //char last[12][5][4][2];
-    //list[0][0][0].matkul = 2;
-    //list[0][0][0].group = 5;
-    //last[0][0][0][0] = 'A';
-    //last[0][0][0][1] = 'B';
-   //for (i=0; i< 12; i++)
-    //{
-        //for (j=0; j < 5; j++)
-        //{
-            //printf("Minggu %d | Hari %s : ", i, getHari(j));
-            //printf("%d|%s|", i + 3, getHari(j));
-            //for (k=0; k< 4; k++)
-            //{
-                //list[i][j][k].matkul = 0;
-                //list[i][j][k].group = 0;
-            //}
-            //printf("\n");
-        //}
-
-    //}
-    loadPraktikum(list);
-    //for (i=0; i< 12; i++)
-    //{
-        //for (j=0; j < 5; j++)
-        //{
-
-            //printf("%d|%s|", i + 3, getHari(j+1));
-            //for (k=0; k < 4; k++)
-            //{
-                //printf("%s%s|", getMatkul(list[i][j][k].matkul), getGroup(list[i][j][k].matkul, list[i][j][k].group));
-            //}
-            //printf("\n");
-        //}
-
-    //}
-    assignPraktikum(list);
-//    saveAsisten(last);
-    savePraktikum(list);
-    return 0;
+	int pilihan1;
+	int pilihan2;
+	while((pilihan1 != 3))
+	{
+		printf("Menu : \n");
+		printf("	1.  Buat Proyek Baru \n");
+		printf("	2.  Muat Proyek Dari Berkas \n");
+		printf("	3.  Keluar \n");
+		printf("Masukan :");scanf("%d", &pilihan1);
+		if((pilihan1 < 1) || (pilihan1 >3))
+		{
+			printf("\n");
+			printf("Input Salah !\n");
+			printf("Pilihan Menu Hanya 1,2, Dan 3 \n");
+			printf("\n");
+		}
+		if((pilihan1 == 1) || (pilihan1 == 2))
+		{
+			if(pilihan1 == 1)
+			{
+				printf("Fungsi new_project \n");
+			}
+			else if(pilihan1 == 2)
+			{
+				printf("Fungsi load_project \n");
+			}
+			printf("\n");
+			while(pilihan2 != 8)
+			{
+				printf("[Menu Utama]\n");
+				printf("Pilih Mode :\n");
+				printf("	1.  Tampilkan Schedule\n");
+				printf("	2.  Scehdule Manual\n");
+				printf("	3.  Tampilkan Status Assignment Asisten\n");
+				printf("	4.  Assign Asisten Manual\n");
+				printf("	5.  Schedule Otomatis\n");
+				printf("	6.  Assign Otomatis\n");
+				printf("	7.  Rule Checker\n");
+				printf("	8.  Simpan Proyek dan Keluar\n");
+				printf("Pilih Mode :");scanf("%d", &pilihan2);
+				if(pilihan2 == 1)
+				{
+					printf("Fungsi print_jadwal \n");
+				}
+				else if(pilihan2 == 2)
+				{
+					printf("Fungsi jadwal_manual \n");
+				}
+				else if(pilihan2 == 3)
+				{
+					printf("Fungsi print_asisten \n");
+				}
+				else if(pilihan2 == 4)
+				{
+					printf("Fungsi asisten_manual \n");
+				}
+				else if(pilihan2 == 5)
+				{
+					printf("Fungsi jadwal_auto \n");
+				}
+				else if(pilihan2 == 6)
+				{
+					printf("Fungsi asisten_auto \n");
+				}
+				else if(pilihan2 == 7)
+				{
+					printf("Fungsi rule_check \n");
+				}
+				else if(pilihan2 == 8)
+				{
+					printf("Fungsi save \n");
+				}
+				else
+				{
+					printf("Input Salah ! \n");
+				}
+			}
+			pilihan2 = 0;
+		}
+	}
+	return 0;
 }
-
-
