07dfce1c1e86346e776a98bc9640b619f69796a7
Arief Hirmanto <41282211+ariefhirmanto@users.noreply.github.com>
2019-04-10 01:56:39 +0700

    nambah fungsi tampilan

--- function.c
+++ function.c
@@ -377,7 +377,7 @@ int getPrakIndex(char *str)
     return index;
 }
 
-//FUNGSI TAMBAHAN
+// ---------------------------------------- FUNGSI TAMBAHAN
 int getHariIndex2(char* str){
   switch (str)
     {
@@ -401,3 +401,53 @@ int getHariIndex2(char* str){
         break;
     }
 }
+
+// ------------------------- FUNGSI TAMPILKAN -----------------------
+int tampilkanSchedule (){
+	int i, j, k;
+	printf ("\n\n\t|--------------------|----------|----------|----------|----------|\n");
+	printf ("\t|   Ruang Praktikum   |   Lab 1  |   Lab 2  |   Lab 3  |    LSS   |\n");
+	printf ("\t|---------------------|----------|----------|----------|----------|\n");
+	printf ("\t|  Minggu  |   Hari   |----------|----------|----------|----------|\n");
+	printf ("\t|----------|----------|----------|----------|----------|----------|\n");
+	for (i=0 ; i<12 ; i++)
+		{
+			for (j=0 ; j<5 ; j++)
+			{
+				printf ("\t|\t%d\t|", [i+3]);
+				printf ("\t%s\t|", getHari(j));
+				for (k=1 ; k=4 ; k++)
+				{
+					printf ("\t%s\t|", listPraktikum[i][j][k]);
+				}
+				printf ("\n");
+			}
+			printf ("\t|-----------------------------------------------------------------|\n");
+		}
+	}
+
+int tampilkanAsisten (){
+	int i, j, k, l;
+	printf ("\n\n\t|--------------------|----------|----------|----------|----------|\n");
+	printf ("\t|   Ruang Praktikum   |   Lab 1  |   Lab 2  |   Lab 3  |    LSS   |\n");
+	printf ("\t|---------------------|----------|----------|----------|----------|\n");
+	printf ("\t|  Minggu  |   Hari   |----------|----------|----------|----------|\n");
+	printf ("\t|----------|----------|----------|----------|----------|----------|\n");
+	for (i=0 ; i<12 ; i++)
+		{
+			for (j=0 ; j<5 ; j++)
+			{
+				printf ("\t|\t%d\t|", [i+3]);
+				printf ("\t%s\t|", getHari(j));
+				for (k=1 ; k=4 ; k++)
+				{
+					for (l=1 ; l=14 ; l++)
+					{
+						printf ("\t%s\t|", listAsisten[i][j][k][l]);
+					}
+				}
+				printf ("\n");
+			}
+			printf ("\t|-----------------------------------------------------------------|\n");
+		}
+	}
