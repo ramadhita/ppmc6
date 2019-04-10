d40a38ec42618fadf75f2247143ad69e927c93ab
harunrasyid98 <49441617+harunrasyid98@users.noreply.github.com>
2019-04-10 10:06:55 +0700

    Update filehandler.h

--- filehandler.h
+++ filehandler.h
@@ -1,3 +1,22 @@
+//***********************************************************//
+//                      [ Source Code ]
+//
+// Institution       : Institut Teknologi Bandung
+// Name              : 
+// Module            : Tugas Besar
+// Problem           : Header file fungsi file handler
+// File Name         : filehandler.h
+// Dependency        : stdio.h, stdlib.h, unistd.h, "function.h", string.h
+//
+// Status:
+//		1.  Arief Himanto / 13217076
+//		2.  Minangkara Ciandi Rengga / 13217087
+//		3.  Kevin Sutardi / 13217088
+//		4.  Ramadhita Umitaibatin / 18317008
+//		5.  Muhammad Harun Al Rasyid / 18317009
+// 
+//***********************************************************//
+
 #ifndef FILEHANDLER_H
 #define FILEHANDLER_H
 #include <stdio.h>
